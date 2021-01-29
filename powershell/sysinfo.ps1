ca21eb630adcd04990c8bdb21ae040cf274a62e0
unknown <flecksj@mail.uc.edu>
2021-01-29 16:38:24 -0500

    sysinfo powershell script

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,9 +1,22 @@
 ﻿#$Hello="Hello, Powershell!"
 #write-HOat($Hello)
-function getIP{
+#Display Computer Name, Date & Time, IP, and PowerSHell Version
+#email self sysinfo
+function getIP {
 (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
+function GetVersion {
+(Get-Host) | Select-Oject Version
+}
+function GetDate {
+(Get-Date -Format "dddd MM/dd/yyyy HH:mm K")
+}
 write-host(getIP)
+$Version=GetVersion
 $IP=getIP
-write-Host("This machine IP is $IP")
-write-Host("This machine's IP is {0}" -f $IP)
+$Date =GetDate
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $. Powershell Version $HOST.Version.Major. Today's date is $Date"
+
+write-host($Body)
+ 
+#Send-MailMessage -To "flecksj@mail.uc.edu" -From "flecksj@mail.uc.edu" -Subject "IT3038C windows SysInfo" -Body $Body -SmtpServer smpt.google.com -port 587 -UseSSL -Credentials (Get-Credential)
